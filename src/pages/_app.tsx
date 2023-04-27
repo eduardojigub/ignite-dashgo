32c0292cd08f6b19009ae4ad441f948e32e904ea
eduardojigub <eduardobrunoit@gmail.com>
2023-04-27 17:58:38 -0300

    feat: intall devtolls, add query from database

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@/styles/theme';
 import { SidebarDrawerProvider } from '@/context/SidebarDrawerContext';
 import { makeServer } from '@/services/mirage';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
