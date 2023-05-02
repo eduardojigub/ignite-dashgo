fb4d04e7a037b3d0a93cc7b851bfb7bcbba5f057
eduardojigub <eduardobrunoit@gmail.com>
2023-05-02 17:57:38 -0300

    feat: using SSR

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,15 +21,18 @@ import {
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { RiAddLine } from 'react-icons/ri';
-import { useUsers } from '@/services/hooks/useUsers';
+import { getUsers, useUsers } from '@/services/hooks/useUsers';
 import { useState } from 'react';
 import { queryClient } from '@/services/queryClient';
 import { api } from '@/services/api';
+import { GetServerSideProps } from 'next';
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
 
-  const { data, isLoading, error, isFetching } = useUsers(page);
+  const { data, isLoading, error, isFetching } = useUsers(page, {
+    initialData: users,
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -136,3 +139,14 @@ export default function UserList() {
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+
+  return {
+    props: {
+      users,
+      totalCount,
+    },
+  };
+};
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { UseQueryOptions, UseQueryResult, useQuery } from "react-query";
 import { api } from "../api";
 
 
@@ -42,12 +42,13 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
     }
   }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
     return useQuery(
         ['users', page],
         () => getUsers(page),
         {
           staleTime: 1000 * 60 * 10, // 10 minutes
+          ...options,
         }
-      );
+      ) as UseQueryResult<GetUsersResponse, unknown>
 }
